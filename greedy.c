0a4b6bd36782b2657cd79454655face37269ca7e
Michael McGuire <mmcg28@gmail.com>
2016-10-05 16:58:01 -0400

    Update greedy.c

--- greedy.c
+++ greedy.c
@@ -8,43 +8,43 @@ int main(void)
     
 
     float change;
-    int coins = 0;
+    int coins = 0;            /*Create a counter for coins*/
 
     do
     {
         printf("How much change is owed?\n");
-        change = GetFloat();
+        change = GetFloat();          /*Get change*/ 
     }
     
-    while(change < 0);
+    while(change < 0);              /*Change must be at least .01*/
 
-    int amount = round(change *100);
+    int amount = round(change *100);   /*Taking the change, multiplying by 100 and rounding it so theres no decimals to deal with*/
 
     while(amount >= 25)
         {
-            coins++;
+            coins++;              /*If amount is less than or equal to 25, subtract 25 and add 1 to coin counter*/
             amount -= 25;
         }
             
     while(amount >= 10)
         {
-            coins++;
+            coins++;              /*repeat for 10*/
             amount -= 10;
         }
         
     while(amount >= 5)
         {
-            coins++;
+            coins++;              /*...*/
             amount -= 5;
         }
         
-    while(amount >= 1)
+    while(amount >= 1)            /*...*/
         {
             coins++;
             amount -= 1;
         }
     
-    printf("%d\n", coins );
+    printf("%d\n", coins );       /*Print the amount of coins*/
 
 return 0;
 }
