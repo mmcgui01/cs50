3f821befd2e3cd7cf1a59d5f09ebc6efe707c28d
Michael McGuire <mmcg28@gmail.com>
2016-10-05 16:52:11 -0400

    Update caeser.c

--- caeser.c
+++ caeser.c
@@ -11,7 +11,7 @@ int main(int argc, string argv[])
     bool valid = false;
     
     
-        if(argc != 2)
+        if(argc != 2)              /*Must only take one argument*/
         {
             
             printf("Try again\n");
@@ -19,33 +19,32 @@ int main(int argc, string argv[])
         }
         else
         {
-            (k = atoi(argv[1]));
-            valid = true;
+            (k = atoi(argv[1]));      /*takes string k and turns it into an integer to be used later*/
+            valid = true;            /**/
         }
         
   
 
-    string text = GetString();
+    string text = GetString();    /*Get text to crypt*/
 
     for(int x = 0, y = strlen(text); x < y; x++)
        {
-           if(isalpha (text[x]))
+           if(isalpha (text[x]))   /*Text must be alpha*/
             {       
-            
-                if(isupper(text[x]))
+              if(isupper(text[x]))  /*If text is upper*/
                     {
-                    printf("%c", (((text[x] - 64) + k)%26) + 64);
-                    }
+                    printf("%c", (((text[x] - 64) + k)%26) + 64);  /*Prints each letter -64 to get to '0' adds k, wrapping it around
+                    }                                             the text up to 26 times for alpha chars. adds 64 to get back to ASCII*/
             
                 else
                     {
-                    printf("%c", (((text[x] - 97) + k)%26) + 97);
+                    printf("%c", (((text[x] - 97) + k)%26) + 97); /*If lower, repeat above, but with 97 which is lowercase a*/
                     }
             }
         
             else
                 {
-                printf("%c",text[x]);
+                printf("%c",text[x]);   /*Print is text is not alpha*/
                 
                 }
        }
